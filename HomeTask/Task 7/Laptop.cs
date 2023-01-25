fde0048e9a64bedbf885750744155ec160e4805b
Aydanshj <121218604+Aydanshj@users.noreply.github.com>
2023-01-26 00:50:06 +0400

    Add files via upload

---
+++ HomeTask/Task 7/Laptop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Xml.Schema;
+
+namespace HomeTask7
+{
+    internal class Laptop
+    {
+        public string Brand;
+        public string Model;
+        public int Price;
+
+        public Laptop(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        public Laptop(string brand, string model, int price = 0)
+        {
+            Price = price;
+            Brand = brand;
+            Model = model;
+        }
+
+        public string showInfo()
+        {
+            return $"Brand {Brand}, Model {Model}, Price {Price}";
+        }
+    }
+}
